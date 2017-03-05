19550820112c581161916daaeb4a9c22e78a7404
jake <jakelwatson86@gmail.com>
2017-03-05 13:30:20 +0000

    Fixing add_notes

--- notebook.py
+++ notebook.py
@@ -39,8 +39,9 @@ class Notebook:
     def _find_note(self, note_id):
         """Locate the note with the given id."""
         for note in self.notes:
-            if note.id == note_id:
+            if str(note.id) == str(note_id):
                 return note
+        return None
 
     def modify_memo(self, note_id, memo):
         """Find the note with the given id and change its memo to the given value."""
